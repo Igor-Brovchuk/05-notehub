8740ed44de7f26602ae0f4073e7d028f9f46578d
Igor Brovchuk <I.Brovchuk@gmail.com>
2026-05-21 11:46:17 +0300

    Refactor noteService to improve API handling

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,9 +1,61 @@
 import axios from 'axios'
-import { Note, NoteTag } from '../types/note'
-
-const API_URL = 'https://notehub-public.goit.study/api'
+import type { Note, NoteTag } from '../types/note'
 
 const token = import.meta.env.VITE_NOTEHUB_TOKEN
 if (!token) {
-  throw new Error('Missing VITE_TMDB_API_TOKEN')
+  throw new Error('Missing VITE_NOTEHUB_TOKEN')
 }
+
+const notehubApi = axios.create({
+  baseURL: 'https://notehub-public.goit.study/api',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+})
+
+interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number
+}
+
+interface FetchNotesParams {
+  page?: number
+  perPage?: number
+  query?: string
+}
+
+interface CreateNoteParams {
+  title: string
+  content: string
+  tag: NoteTag
+}
+
+export const fetchNotes = async ({
+  page = 1,
+  perPage = 12,
+  query = '', 
+}: FetchNotesParams): Promise<FetchNotesResponse> => {
+  const response = await notehubApi.get<FetchNotesResponse>('/notes', {
+    params: {
+      page,
+      perPage,
+      search: query.trim() || undefined,
+    },
+  })
+  return response.data
+}
+
+export const createNote = async (
+  newNote: CreateNoteParams
+): Promise<Note> => {
+  const response = await notehubApi.post<Note>('/notes', newNote)
+
+  return response.data
+}
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const response = await notehubApi.delete<Note>(`/notes/${id}`)
+
+  return response.data
+}
+
